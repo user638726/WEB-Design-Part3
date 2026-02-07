ddf04711be0b804243bd7286cfe42b787194caf2
user638726 <lienjackson35@gmail.com>
2026-02-08 00:34:59 +0800

    建構網站part4

--- js/all.js
+++ js/all.js
@@ -7,3 +7,14 @@ $(function () {
     $('.header').removeClass('is-open');
   });
 });
+const asideItems = document.querySelectorAll('.aside-bar ul li');
+
+  asideItems.forEach(item => {
+    item.addEventListener('click', () => {
+      // 移除所有 active
+      asideItems.forEach(li => li.classList.remove('active'));
+
+      // 幫被點擊的加上 active
+      item.classList.add('active');
+    });
+  });
